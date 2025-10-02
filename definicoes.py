5eff47fff6615f2ca1f49e37f00127138bde35e7
Thales Costa Queiroga <thalescq@diasclinica.intra>
2025-10-02 16:49:33 -0300

    Atualizando projeto

---
+++ definicoes.py
@@ -0,0 +1,48 @@
+import os
+from musica import app
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators, SubmitField, PasswordField
+
+
+class FormularioMusica(FlaskForm):
+
+    nome = StringField('Nome da música', [validators.DataRequired(), 
+                                          validators.length(min=2, max=50)])
+    
+    grupo = StringField('Cantor / Banda /Grupo', [validators.DataRequired(),
+                                                   validators.length(min=2, max=50)])
+
+    genero = StringField('Genero', [validators.DataRequired(),
+                                                   validators.length(min=2, max=20)])
+    
+    cadastrar = SubmitField('Cadastrar')
+
+
+class FormularioUsuario(FlaskForm):
+    
+    usuario = StringField('Usuário', [validators.DataRequired(),
+                                  validators.length(min=2, max=20)])
+    
+    senha = PasswordField('Senha', [validators.DataRequired(),
+                                    validators.length(min=4, max=15)])
+    
+    logar = SubmitField('Entrar')
+    
+
+def recupera_imagem(id):
+    for nome_imagem in os.listdir(app.config['UPLOAD_PASTA']):
+
+        nome = str(nome_imagem)
+
+        nome = nome.split('.')
+
+        if f'album{id}_' in nome[0]:
+            return nome_imagem
+    
+    return 'default.png'
+
+def deletarImagem(id):
+    imagem = recupera_imagem(id)
+
+    if imagem != 'default.png':
+        os.remove(os.path.join(app.config['UPLOAD_PASTA'], imagem))
--- views.py
+++ views.py
@@ -1,50 +1,46 @@
-from flask import render_template, request, redirect,session, flash, url_for
+from flask import render_template, request, redirect,session, flash, url_for, send_from_directory
 from models import Musica, Usuario
 from musica import db, app
+from definicoes import recupera_imagem, deletarImagem, FormularioMusica, FormularioUsuario
+import time
 
 
-@app.route('/editar/<int:id>')
-def editarMusica(id):
+#--------Home-----------------------------------------------------------------
+
+
+@app.route('/')
+def homePage():
+    
     if 'usuario_logado' not in session or session['usuario_logado'] is None:
         return redirect(url_for('loginMusicas'))
-    
-    musicaBuscada = Musica.query.filter_by(id_musica=id).first()
-    
-    return render_template('editar_musica.html', musica = musicaBuscada)
 
 
-@app.route('/atualizar', methods=['POST',])
-def atualizar():
-    if 'usuario_logado' not in session or session['usuario_logado'] is None:
-        return redirect(url_for('loginMusicas'))
-    
-    musica = Musica.query.filter_by(id_musica=request.form['txtId']).first()
-    
-    musica.nome_musica = request.form['txtNome']
-    musica.cantor_musica = request.form['txtCantorGrupo']
-    musica.genero_musica = request.form['txtGenero']
+    return render_template('base.html')
 
-    db.session.add(musica)
+#--------LOGIN-----------------------------------------------------------------
 
-    db.session.commit()
-    return redirect(url_for('listaMusicas'))
 
 @app.route('/login')
 def loginMusicas():  
 
-    return render_template('login.html')
+    form = FormularioUsuario()
+
+    return render_template('login.html', form = form)
 
 
 @app.route('/autenticar', methods=['POST',])
 def autenticarUsuario():
 
-    usuario = Usuario.query.filter_by(login_usuario = request.form['txtLogin']).first()
+    form = FormularioUsuario(request.form)
+
+    
+    usuario = Usuario.query.filter_by(login_usuario = form.usuario.data) .first()
 
     if usuario:
 
-        if request.form['txtSenha'] == usuario.senha_usuario:
+        if form.senha.data == usuario.senha_usuario:
            
-            session['usuario_logado'] = request.form['txtLogin']
+            session['usuario_logado'] = usuario.login_usuario
 
             flash(f"{usuario.nome_usuario} logado com sucesso!")
 
@@ -57,28 +53,8 @@ def autenticarUsuario():
          
          flash("Usuário ou senha invalido!")
          return redirect(url_for('loginMusicas'))
-    
 
-@app.route('/')
-def homePage():
-    
-    if 'usuario_logado' not in session or session['usuario_logado'] is None:
-        return redirect(url_for('loginMusicas'))
-
-
-    return render_template('base.html')
-
-
-@app.route('/lista')
-def listaMusicas():
-    
-    if 'usuario_logado' not in session or session['usuario_logado'] is None:
-        return redirect(url_for('loginMusicas'))
-       
-
-    lista = Musica.query.order_by(Musica.id_musica)
-
-    return render_template('lista_musicas.html', musicas = lista)
+#--------CADASTRO--------------------------------------------------------------
 
 
 @app.route('/cadastro')
@@ -87,8 +63,10 @@ def cadastroMusicas():
     if 'usuario_logado' not in session or session['usuario_logado'] is None:
         return redirect(url_for('loginMusicas'))
     
+    form = FormularioMusica()
+    
 
-    return render_template('cadastra_musica.html')
+    return render_template('cadastra_musica.html', form = form)
 
 
 @app.route('/adicionar', methods=['POST',])
@@ -97,10 +75,15 @@ def adicionarMusica():
     if 'usuario_logado' not in session or session['usuario_logado'] is None:
         return redirect(url_for('loginMusicas'))
     
+    
+    formRecebido = FormularioMusica(request.form)
 
-    nome = request.form['txtNome']
-    cantorGrupo = request.form['txtCantorGrupo']
-    genero = request.form['txtGenero']
+    if not formRecebido.validate_on_submit():
+        return redirect(url_for('cadastroMusicas'))
+
+    nome = formRecebido.nome.data
+    cantorGrupo = formRecebido.grupo.data
+    genero = formRecebido.genero.data
 
     musica = Musica.query.filter_by(nome_musica = nome).first()
 
@@ -115,6 +98,122 @@ def adicionarMusica():
     
     db.session.commit()
 
+    arquivo = request.files['txtArquivo']
+
+    if arquivo:
+
+
+        pasta_arquivos = app.config['UPLOAD_PASTA']
+
+        nome_arquivo  = arquivo.filename
+
+        nome_arquivo = nome_arquivo.split('.')
+
+        extensao = nome_arquivo[len(nome_arquivo)-1]
+
+        momento = time.time()
+
+        nome_completo = f'album{nova_musica.id_musica}_{momento}.{extensao}'
+
+        arquivo.save(f'{pasta_arquivos}/{nome_completo}')
+
+    return redirect(url_for('listaMusicas'))
+
+#---------LISTA----------------------------------------------------------------
+
+
+@app.route('/lista')
+def listaMusicas():
+    
+    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+        return redirect(url_for('loginMusicas'))
+       
+
+    lista = Musica.query.order_by(Musica.id_musica)
+
+
+    return render_template('lista_musicas.html', musicas = lista)
+
+
+@app.route('/editar/<int:id>')
+def editarMusica(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+        return redirect(url_for('loginMusicas'))
+    
+    musicaBuscada = Musica.query.filter_by(id_musica=id).first()
+
+    form = FormularioMusica()
+
+    form.nome.data = musicaBuscada.nome_musica
+    form.grupo.data = musicaBuscada.cantor_musica
+    form.genero.data = musicaBuscada.genero_musica
+
+    album = recupera_imagem(id)
+    
+    return render_template('editar_musica.html',
+                            musica = form, album_musica = album, id=id)
+
+
+@app.route('/atualizar', methods=['POST',])
+def atualizar():
+    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+        return redirect(url_for('loginMusicas'))
+    
+    formRecebido = FormularioMusica(request.form)
+
+    if formRecebido.validate_on_submit():
+    
+        musica = Musica.query.filter_by(id_musica=request.form['txtId']).first()
+    
+        musica.nome_musica = formRecebido.nome.data
+        musica.cantor_musica = formRecebido.grupo.data
+        musica.genero_musica = formRecebido.genero.data
+
+        db.session.add(musica)
+
+        db.session.commit()
+
+        arquivo = request.files['txtArquivo']
+    
+        if arquivo:
+            pasta_upload = app.config['UPLOAD_PASTA']
+
+            nome_arquivo = arquivo.filename
+
+            nome_arquivo = nome_arquivo.split('.')
+            
+            extensao = nome_arquivo[len(nome_arquivo)-1]
+
+            
+            momento = time.time()
+
+            nome_completo = f'album{musica.id_musica}_{momento}.{extensao}'
+            
+            deletarImagem(musica.id_musica)
+
+            arquivo.save(f'{pasta_upload}/{nome_completo}')
+
+        
+        flash("Musica editada com sucesso!")
+
+    return redirect(url_for('listaMusicas'))
+
+
+@app.route('/excluir/<int:id>')
+def excluirMusica(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+        return redirect(url_for('loginMusicas'))
+    
+    
+    Musica.query.filter_by(id_musica=id).delete()
+
+    deletarImagem(id)
+
+    db.session.commit()
+
+    flash ("Musica excluida com sucesso")
+
+
     return redirect(url_for('listaMusicas'))
 
 
@@ -123,3 +222,8 @@ def sair():
     session['usuario_logado'] = None
 
     return redirect('/login')
+
+
+@app.route('/uploads<nome_imagem>')
+def imagem(nome_imagem):
+    return send_from_directory('uploads', nome_imagem)
